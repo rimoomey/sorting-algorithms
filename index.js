3fe31c02dfb5818d98112abfb00ec556f1a08343
Rimon Moomey <rimon.moomey@gmail.com>
2022-11-29 15:23:28 -0600

    add selectionSort and pseudocode

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
 import { bubbleSort } from "./scripts/bubbleSort.js";
+import { selectionSort } from "./scripts/selectionSort.js";
 
+console.log("BUBBLE: ");
 console.log(bubbleSort([2, 1, 3, 4, 5]));
 console.log(bubbleSort([2, 1, 4, 5, 3]));
+console.log(
+  "Average time in milliseconds for array of length 100: ",
+  sortBenchmark(bubbleSort),
+  "\n"
+);
+
+console.log("SELECTION: ");
+console.log(selectionSort([2, 1, 3, 4, 5]));
+console.log(selectionSort([2, 1, 4, 5, 3]));
+console.log(
+  "Average time in milliseconds for array of length 100: ",
+  sortBenchmark(selectionSort),
+  "\n"
+);
+
+function sortBenchmark(fn) {
+  const beginning = new Date().getTime();
+  for (let i = 0; i < 1000; i++) {
+    fn([
+      71, 21, 96, 52, 4, 10, 57, 23, 58, 46, 13, 33, 63, 94, 9, 3, 1, 44, 24,
+      64, 95, 40, 99, 83, 81, 62, 59, 82, 80, 2, 11, 45, 47, 28, 73, 98, 30, 5,
+      26, 93, 84, 65, 32, 20, 89, 53, 76, 55, 69, 54, 17, 67, 42, 27, 43, 41,
+      51, 6, 37, 25, 15, 90, 31, 87, 86, 48, 19, 34, 49, 100, 29, 85, 50, 97,
+      39, 61, 79, 18, 16, 78, 77, 8, 38, 7, 66, 88, 75, 60, 72, 70, 12, 35, 36,
+      74, 68, 92, 22, 56, 14, 91,
+    ]);
+  }
+  const end = new Date().getTime();
+
+  return (end - beginning) / 2000;
+}
--- scripts/selectionSort.js
+++ scripts/selectionSort.js
@@ -1,5 +1,47 @@
 function selectionSort(arr) {
+  for (let currentIndex = 0; currentIndex < arr.length - 1; currentIndex++) {
+    let minimumIndex = findMinimum(arr.slice(currentIndex));
+    minimumIndex += currentIndex;
 
+    if (arr[minimumIndex] < arr[currentIndex]) {
+      const temp = arr[currentIndex];
+      arr[currentIndex] = arr[minimumIndex];
+      arr[minimumIndex] = temp;
+    }
+  }
+  return arr;
 }
 
-export default selectionSort;
+function findMinimum(arr) {
+  let minimum = Number.MAX_VALUE;
+  let minimumIndex = -1;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < minimum) {
+      minimum = arr[i];
+      minimumIndex = i;
+    }
+  }
+  return minimumIndex;
+}
+
+export { selectionSort };
+
+/**
+ * the idea behind selection sort is that you find the minimum value in the array and swap it into the first index
+ * then, you calculate the minimum value of array.slice(1), i.e. the rest of the array, and swap it into the second index
+ * this continues until you reach the end of the array
+ *
+ * pseudocode
+ *
+ * function selectionSort(arr) {
+ *
+ *   for(let i = 0; i < arr.length; i++) {
+ *     let minimumIndex = index of minimum value from i to arr.length - 1 (arr.slice(i))
+ *
+ *     swap minimumIndex value and i value
+ *   }
+ *
+ *  return arr
+ * }
+ */
